e15adb5a63052974754ef6222c53d8e401ee0e69
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-06-27 20:54:07 -0300

    feat: atualizando models.py

--- catalog/models.py
+++ catalog/models.py
@@ -19,6 +19,10 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    sexo = models.CharField(max_length=2, choices=[('ML', 'Masculino'), ('FM', 'Feminino'), ('UN', 'Unissex')])
+    imagem = models.ImageField(upload_to='product_images/')
 
     
     def __str__(self):
