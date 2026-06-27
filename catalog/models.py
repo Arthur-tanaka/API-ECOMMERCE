a284896c82e1a757d8f5809b234ba6055899d758
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-06-27 00:55:45 -0300

    feat: primeiro commit, criando estrutura

---
+++ catalog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    
+    def __str__(self):
+        return self.name
+    
+    
