1e136f8e2ddc056a60a3352166843ef6d95256de
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-06-29 20:27:25 -0300

    feat: implementação dos Models CartItem e Cart

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.conf import settings
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
     
@@ -24,8 +26,22 @@ class Product(models.Model):
     sexo = models.CharField(max_length=2, choices=[('ML', 'Masculino'), ('FM', 'Feminino'), ('UN', 'Unissex')])
     imagem = models.ImageField(upload_to='product_images/')
 
-    
     def __str__(self):
         return self.name
     
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"Cart {self.id}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        if self.product is not None:
+            return f"A quantidade de {self.product.name} é {self.quantity}"
+        else:
+            return f"Produto removido"
